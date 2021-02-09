83ee578643af19ae350ea273f107e81316917c47
RyzenJS <ryzenjs@gmail.com>
2021-02-10 04:37:39 +0800

    amber template work in progress

---
+++ resources/views/characters/amber.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <!--content here-->
+<div class="container h-full">
+    <div class="grid grid-cols-3 gap-4">
+        <div class="md:col-span-3 ...">
+            <div class="bg-gray-600 p-2 rounded center">
+                    <img src="https://rerollcdn.com/GENSHIN/Elements/Element_Pyro.png" md:bg-left-top width="30" height="30" class="character-type" alt="Cryo">
+                    <img src="https://rerollcdn.com/GENSHIN/Characters/Amber.png" alt="Amber" class="center">
+              </div>
+        </div>
+     </div>
+</div>
+@endsection
